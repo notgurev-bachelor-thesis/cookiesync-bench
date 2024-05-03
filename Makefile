ee06e06ea108a132ed8bcfdb3d236c5377141280
notgurev <notgurev@gmail.com>
2024-05-03 12:47:01 +0300

    Impl bench

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-.PHONY: remote
+.PHONY: remote deploy build
 
 remote:
 	ssh root@spb-w3-stathandler.moevideo.net
 
-deploy:
+deploy: build
 	scp -r * root@spb-w3-stathandler.moevideo.net:~/bench
+
+build:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bench
--- main.go
+++ main.go
@@ -1,13 +1,63 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"math/rand"
+	"sync"
+)
 
 func main() {
-	if err := app(); err != nil {
-		log.Fatalln(err)
-	}
+	url := "http://cl-hot1-1.moevideo.net:8080" // Target server URL
+	requests := 100                             // Number of requests per connection
+	connections := 10                           // Number of concurrent connections
+
+	generateLoad(url, requests, connections)
 }
 
-func app() error {
-	return nil
+func generateRandomUID(length int) string {
+	const charset = "0123456789abcdef"
+	uid := make([]byte, length)
+	for i := range uid {
+		uid[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(uid)
+}
+
+func generateLoad(url string, requests int, connections int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < connections; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for j := 0; j < requests; j++ {
+				uid := generateRandomUID(20)
+				d := rand.Intn(20) + 1
+				b := rand.Intn(1000000) + 1
+
+				reqURL := fmt.Sprintf("%s?d=%d&b=%d", url, d, b)
+
+				req := fasthttp.AcquireRequest()
+
+				req.SetRequestURI(reqURL)
+				req.Header.SetCookie("uid", uid)
+
+				resp := fasthttp.AcquireResponse()
+
+				if err := fasthttp.Do(req, resp); err != nil {
+					fmt.Printf("Error: %s\n", err)
+					return
+				}
+
+				fasthttp.ReleaseRequest(req)
+				fasthttp.ReleaseResponse(resp)
+
+				fmt.Printf("Request %d: Status %d, UID %s, d %d, b %d\n", j+1, resp.StatusCode(), uid, d, b)
+			}
+		}()
+	}
+
+	wg.Wait()
 }
